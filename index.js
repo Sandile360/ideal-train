44885ee825ae1644dfebf53d3b0c28cc001e9020
Sandile360 <Sandilendlovucgi360@gmail.com>
2025-04-22 08:29:09 +0200

    series api created and tested with insomnia

--- index.js
+++ index.js
@@ -2,11 +2,91 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const Movie = require('./api/movies.js');
+const Series = require('./api/series.js');
+const Song = require('./api/songs.js');
 
 const app = express();
 app.use(express.json());
 
+// series
+app.get('/series', async(req, res) => {
 
+    try{
+        const series = await Series.find({});
+        res.status(200).json(series);
+    }catch(err){
+        console.log(err);
+        res.status(500).send('Error Getting Series');
+    }
+}
+);
+
+app.get('/series/:id', async(req, res) => {
+
+    try{
+        const {id} = req.params;
+        const series = await Series.findById(id);
+        res.status(200).json(series);
+
+    }catch(err){
+        console.log(err);
+        res.status(500).send('Error Getting Series');
+    }
+}
+);
+
+app.post('/series', async(req, res) => {
+
+    try{
+        const movie = await Series.create(req.body);
+        res.status(200).json(movie);
+        
+    }catch(err){
+        console.log(err);
+        res.status(404).send('Error creating Series');
+    }
+}
+);
+
+app.put('/series/:id', async(req, res) => {
+
+    try{
+        const {id} = req.params;
+        const series = await Series.findByIdAndUpdate(id,req.body, {new: true});
+        if(!series){
+            return res.status(404).send('Series not found');
+        }
+        const updatedSeries = await Series.findById(id);
+        res.status(200).json(updatedSeries);
+
+
+    }catch(err){
+        console.log(err);
+        res.status(500).send('Error creating Series');
+    }
+}
+);
+
+
+app.delete('/series/:id', async(req, res) => {
+
+    try{
+        const {id} = req.params;
+        const series = await Series.findByIdAndDelete(id,req.body);
+        if(!series){
+            return res.status(404).send('Series not found');
+        }
+        res.status(200).json(series);
+
+    }catch(err){
+        console.log(err);
+        res.status(500).send('Error deleting Series');
+    }
+}
+);
+
+
+// Movies
 app.get('/movies', async(req, res) => {
 
     try{
